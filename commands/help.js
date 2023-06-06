7dcc0fed0c99bf1fa1c2f4d2b0c167aa9051daf8
simplysabir <simplysabir@gmail.com>
2023-06-06 09:00:38 +0530

    🥁 feat : Added help and other commands

---
+++ commands/help.js
@@ -0,0 +1,28 @@
+#!usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+
+const sleep = (ms = 2000) => new Promise((r)=>setTimeout(r, ms));
+
+async function help(){
+    const rainbowTitle = chalkAnimation.karaoke('Here is the list of commands available.. \n').start();
+    await sleep();
+    rainbowTitle.stop();
+
+    console.log(`
+    ${chalk.bgYellow('cnft')} : "For getting all the resources related to compressed NFTs.",
+
+    ${chalk.bgYellow('faucet')} : "For getting all the resources related to getting devnet faucet.",
+
+    ${chalk.bgYellow('learn')} : "For getting all the resources to learn solana development. ",
+
+    ${chalk.bgRedBright('note')} : "Type commands like this ${chalk.red('solanakit command_name')}",
+    `)
+}
+
+await help();
--- index.js
+++ index.js
@@ -7,4 +7,23 @@ import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
 
-console.log(chalkAnimation.rainbow("Welcome to Solana Kit ⚒️").start());
+// const rainbowTitle = chalkAnimation.rainbow('Who Wants to be a JavaScript Millionare? \n');
+// await sleep();
+// rainbowTitle.stop();
+
+
+const sleep = (ms = 2000) => new Promise((r)=>setTimeout(r, ms));
+
+async function welcome(){
+    // console.clear();
+    const rainbowTitle = chalkAnimation.rainbow('Thank you for Using solanakit. \n').start();
+    await sleep();
+    rainbowTitle.stop();
+
+}
+
+function intro(){
+    console.log(`You can Type ${chalk.bgGreenBright(' solanakit help ')} to list down all of the available commands.`)
+}
+await welcome();
+intro();
